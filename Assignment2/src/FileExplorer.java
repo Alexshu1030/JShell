2ed5da3d48e66965925f6faccc4aff81e293e9cf
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-18 03:01:44 +0000

    Created the FileExplorer class and its method signatures.

---
+++ Assignment2/src/FileExplorer.java
@@ -0,0 +1,28 @@
+
+public class FileExplorer {
+
+  //public static Directory root;
+  
+  // Default constructor
+  public FileExplorer() {
+    
+  }
+  
+  // Returns the file at the given path
+  public File GetFile(string path) {
+    
+    return null;
+  }
+  
+  // Adds the file at the given path
+  public void AddFile(string path, File file) {
+    
+    return;
+  }
+  
+  // Returns whether that path exists in the file explorer
+  public boolean PathExists(string path) {
+    
+    return false;
+  }
+}
