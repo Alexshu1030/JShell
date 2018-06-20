3c17c569240781123cb231943a599724962f1d3a
tanuanje <tanuanje@UTORONTO.CA>
2018-06-20 17:00:05 +0000

    added prompt, validity check, split

--- driver/JShell.java
+++ driver/JShell.java
@@ -10,7 +10,7 @@
 // UT Student #: 1003939982
 // Author: Jeremy Tanuan
 //
-// Student3:
+// Student3: 
 // UTORID user_name:
 // UT Student #:
 // Author:
@@ -29,11 +29,175 @@
 // *********************************************************
 package driver;
 
+import java.util.Scanner;
+
 public class JShell {
+
+  private static String prompt = "/# ";
+  private static String errorMessage = "Invalid command, please try again";
+
+  private static String[] commandNames = new String[] {"exit", "mkdir", "cd", 
+      "ls", "pwd", "mv", "cp", "cat", "get", "echo"};
+  private static int[] commandArgs = new int[] {0, 1, 1, 0, 0, 2, 2, 1, 1, 3};
 
   public static void main(String[] args) {
     // TODO Auto-generated method stub
 
+    Scanner in = new Scanner(System.in);
+    boolean terminate = false;
+
+    while (!terminate) {
+
+      System.out.print(prompt);
+      String input = in.nextLine();
+      input = input.trim();
+      String[] splitInput = Split(input);
+
+      int argumentsLen = splitInput.length;
+
+      // Check if the input is a valid command
+      if (IsValidCommand(splitInput)) {        
+        // It is the exit command. Leave the loop.
+        if (splitInput[0].equals(commandNames[0])) {
+          terminate = true;
+        } else {
+          // Print the command
+          System.out.println(splitInput[0]);
+
+          if (argumentsLen > 1) {
+            for (int i = 1; i < argumentsLen; i++) {
+
+              System.out.print(splitInput[i]);
+
+              // Adds spaces between the arguments
+              if (i < argumentsLen - 1) {
+                System.out.print(" ");
+              }
+            }
+
+            // End this line
+            System.out.println();
+          }
+          else {
+            // The command has no arguments and we need to
+            // print a blank line
+            System.out.println();
+          }
+        }
+      }
+      else {
+        // The command is not valid. Print the error message.
+        System.out.println(errorMessage);
+      }
+    }
   }
 
+  private static String[] Split(String text) {
+
+    boolean inQuotes = false;
+    boolean atTextEnd = false;
+
+    int splits = 0;
+
+    for (int i = 0; i < text.length(); i++) {
+
+      if (text.charAt(i) == '"') {
+
+        if (!inQuotes && atTextEnd)
+          atTextEnd = false;
+
+        inQuotes = !inQuotes;
+      }
+
+      if (!inQuotes) {
+        if (atTextEnd) {
+          if (text.charAt(i) != ' ')
+            atTextEnd = false;
+        }
+        if (text.charAt(i) == ' ' || i == text.length() - 1) {
+          atTextEnd = true;
+          splits++;
+        }
+      }
+    }
+
+
+    inQuotes = false;
+    atTextEnd = false;
+
+    int currentSplit = 0;
+    String[] splitText = new String[splits];
+
+    int beginIndex = 0;
+
+    for (int i = 0; i < text.length(); i++) {
+
+      if (text.charAt(i) == '"') {
+
+        if (!inQuotes) {
+          atTextEnd = false;
+          beginIndex = i;
+        }
+        inQuotes = !inQuotes;
+      }
+
+      if (!inQuotes) {
+        if (atTextEnd) {
+          if (text.charAt(i) != ' ') {
+            beginIndex = i;
+            atTextEnd = false;
+          }
+        }
+        else {
+          if (text.charAt(i) == ' ') {
+            atTextEnd = true;        
+            splitText[currentSplit] = text.substring(beginIndex, i);              
+            currentSplit++;
+          }
+        }
+
+      }
+      if (i == text.length() - 1) {    
+        splitText[currentSplit] = text.substring(beginIndex);
+      }
+    }
+    return splitText;
+  }
+
+  private static boolean IsValidCommand(String[] input) {
+
+    boolean isCommand = false;
+    int inputLen = input.length;
+
+    // If there is no input then it can't be a command
+    if (inputLen > 0) {
+      // Check if the first split matches any of the commands
+      for (int i = 0; i < commandNames.length; i++) {
+        if (commandNames[i].equals(input[0])) {
+          // A command has entered. Now we have to make sure that there are
+          // the correct number of arguments
+          if (commandArgs[i] == inputLen - 1) {
+
+            // If it is echo command then the second arg must be the > or >>
+            if (commandNames[i].equals(commandNames[9])) {
+              int secArgLen = input[1].length();
+              if (input[2].equals(">>") || input[2].equals(">")) {
+                // echo's STRING input must have quotations around it
+                if (input[1].substring(0, 1).equals("\"") &&
+                    input[1].substring(secArgLen - 1, secArgLen).equals("\"")) {
+                  isCommand = true;
+                }
+              }
+            }
+            else {
+              // The command name is correct and there are the right number of
+              // arguments. This is a command.
+              isCommand = true;
+            }
+          }
+        }
+      }
+    }
+    return isCommand;
+  }
 }
