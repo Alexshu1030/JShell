a9d64424fb077aee8aff07b5d996541a475605c9
hogancia <hogancia@UTORONTO.CA>
2018-06-21 01:00:32 +0000

    Implemented the methods.

--- Path.java
+++ Path.java
@@ -3,21 +3,32 @@ public class Path {
   
   public static String getRootDirectory(String path) {
     
-    return "";
+    int firstSlash = path.indexOf('/');
+    
+    return path.substring(0, firstSlash);
   }
   
   public static String getFileName(String path) {
         
-    return "";
+    int decimalIndex = path.indexOf('.');
+    int nameStartIndex = path.lastIndexOf('/');
+    
+    return path.substring(nameStartIndex, decimalIndex);
   }
   
   public static String getFileExtension(String path) {
 
-    return "";
+    int decimalIndex = path.indexOf('.');
+    
+    return path.substring(decimalIndex);
   }
   
   public static boolean isAbsolute(String path) {
     
-    return false;
+    // Get the first character in the path
+    char firstChar = path.charAt(0);
+   
+    // If the first character is a slash then it is an absolute path.
+    return firstChar == '/';
   }
 }
