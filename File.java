9491227b5a3ada0aebf19bf36701f08654257bfd
wuaster <wuaster@UTORONTO.CA>
2018-06-21 00:05:42 +0000

    Replaced generics with Object class and added honour code.

--- File.java
+++ File.java
@@ -1,8 +1,37 @@
-public class File<T> {
+// **********************************************************
+// Assignment2:
+// Student1: Ciaran Hogan
+// UTORID user_name: hogancia
+// UT Student #: 1003796859
+// Author: Ciaran Hogan
+//
+// Student2: Jeremy Tanuan
+// UTORID user_name: tanuanje
+// UT Student #: 1003939982
+// Author: Jeremy Tanuan
+//
+// Student3: Aster Wu
+// UTORID user_name: wuaster
+// UT Student #: 1004175117
+// Author: Aster Wu
+//
+// Student4: Yan Chen Huang
+// UTORID user_name: huan1085
+// UT Student #: 1004325857
+// Author: Yan Chen Huang
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences.
+// *********************************************************
+public class File {
   private String fileName;
   private Directory fileDirectory;
-  private T fileContents;
-  public File(String name, String directory, T contents) {
+  private Object fileContents;
+  public File(String name, String directory, Object contents) {
     // Constructor for file class
     setFileName(name);
     setFileDirectory(directory);
@@ -15,7 +44,7 @@ public class File<T> {
   private void setFileDirectory(Directory directory) {
     this.fileDirectory = directory;
   }
-  private void setFileContents(T contents) {
+  private void setFileContents(Object contents) {
     this.fileContents = contents;
   }
   
@@ -26,7 +55,7 @@ public class File<T> {
   public Object getFileDirectory() {
     return this.fileDirectory;
   }
-  public String getFileContents() {
+  public Object getFileContents() {
     return this.fileContents;
   }
   public boolean isDirectory() {
