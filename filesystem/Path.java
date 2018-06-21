3844015b8e509da3f54ad10ed84ea12645ea482f
hogancia <hogancia@UTORONTO.CA>
2018-06-21 02:14:57 +0000

    Fixed a couple of bugs.

--- filesystem/Path.java
+++ filesystem/Path.java
@@ -2,17 +2,39 @@ package filesystem;
 
 public class Path {
   
-  public static String getRootDirectory(String path) {
+  public static String getSubPath(String path) {
     
     int firstSlash = path.indexOf('/');
     
-    return path.substring(0, firstSlash);
+    String result = "";
+    
+    if (firstSlash != -1)
+      result = path.substring(firstSlash + 1);
+    
+    return result;
+  }
+  
+  public static String getRootDirectory(String path) {
+    
+    // If the path is absolute then we want to ignore the first character
+    // (which is a slash)
+    if (isAbsolute(path))
+      path = path.substring(1);
+      
+    int firstSlash = path.indexOf('/');
+    
+    String result = "";
+    
+    if (firstSlash != -1)
+      result = path.substring(0, firstSlash);
+    
+    return result;
   }
   
   public static String getFileName(String path) {
         
     int decimalIndex = path.indexOf('.');
-    int nameStartIndex = path.lastIndexOf('/');
+    int nameStartIndex = path.lastIndexOf('/') + 1;
     
     return path.substring(nameStartIndex, decimalIndex);
   }
