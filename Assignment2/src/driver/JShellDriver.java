ab0841c93bd5b3ed39e20ecf530aa7dda17213a7
tanuanje@UTORONTO.CA <tanuanje@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-21 00:04:31 +0000

    added JShellDriver

---
+++ Assignment2/src/driver/JShellDriver.java
@@ -0,0 +1,8 @@
+public class JShellDriver {
+  
+  public static void main(String[] args) {
+    System.out.println("test working");
+    JShell shell1 = new JShell();
+    JShell.runJShell();
+  }
+}
