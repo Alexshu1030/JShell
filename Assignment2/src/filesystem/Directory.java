877cab03f7fbb639c326ad8e2f293c52d40fd65d
huan1085@UTORONTO.CA <huan1085@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-21 02:16:58 +0000

    added comments for directory

--- Assignment2/src/filesystem/Directory.java
+++ Assignment2/src/filesystem/Directory.java
@@ -3,22 +3,27 @@ package filesystem;
 import java.util.ArrayList;
 
 public class Directory extends File {
+  // make a new arraylist of files 
   ArrayList<File> listOfFiles = new ArrayList<File>();
+  // construct a new directory with its contents as the list of files
   public Directory (String name, Directory parentDirectory) {
     super(name, parentDirectory, null);
     this.fileContents = listOfFiles;
   }
   
+  // add a file to the arraylist of files
   public void addFile(File file) {
     listOfFiles.add(file);
     this.fileContents = listOfFiles;
   }
   
+  // remove a file from the arraylist of files
   public void removeFile(File file) {
     listOfFiles.remove(file);
     this.fileContents = listOfFiles;
   }
   
+  // return true since the object is a directory
   public boolean isDirectory() {
     return true;
   }
