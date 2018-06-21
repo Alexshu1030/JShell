0b7858191a64ae4c805212e4431a45c53294a432
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-21 01:50:23 +0000

    Completed the getDirectory method.

--- Assignment2/src/filesystem/FileExplorer.java
+++ Assignment2/src/filesystem/FileExplorer.java
@@ -1,8 +1,12 @@
 package filesystem;
 
+import java.util.ArrayList;
+
 public class FileExplorer {
 
-  private static Directory root;
+  private static Directory rootDirectory;
+  
+  private Directory workingDirectory;
   
   // Default constructor
   public FileExplorer() {
@@ -12,14 +16,70 @@ public class FileExplorer {
   
   public static Directory getRootDirectory() {
     
-    return root;
+    return rootDirectory;
+  }
+  
+  public Directory getWorkingDirectory() {
+    
+    return workingDirectory;
+  }
+  
+  public void setWorkingDirectory(Directory newWD) {
+    
+    workingDirectory = newWD;
   }
   
   // Returns the file at the given path
   public File getFile(String path) {
     
+    String nextDirectory = Path.getRootDirectory(path);
+    String subPath = Path.getSubPath(path);
+    
+    if (Path.isAbsolute(path)) {
+      // Then the path is relative to the rootDirectory
+    }
+    else {
+      // The path is relative to the workingDirectory
+    }
+    
     return null;
   }
+  
+  private Directory getDirectory(Directory startDirectory, String relativePath) {
+    
+    String nextDirName = Path.getRootDirectory(relativePath);
+    String subPath = Path.getSubPath(relativePath);
+    
+    ArrayList<File> files = (ArrayList<File>)startDirectory.getFileContents();
+    
+    int index = 0;
+    Directory nextDirectory = null;
+    
+    // Iterate over the current directory looking for the file with the name
+    // of the next directory
+    while (index < files.size() && nextDirectory == null) {
+      
+      // Get the name of the current file we are looking at
+      String fileName = files.get(index).getFileName();
+      
+      // If we have found the next directory exit the loop. Otherwise go to
+      // the next file in the current directory.
+      if (fileName.equals(nextDirName))       
+        nextDirectory = (Directory)files.get(index);
+      else
+        index++;
+    }
+    
+    // If we were able to find the next directory then go to it. Otherwise the
+    // path does not exist.
+    Directory result = null;
+    
+    if (nextDirectory != null)
+      result = getDirectory(nextDirectory, subPath);
+    
+    return result;
+  }
+
   
   // Adds the file at the given path
   public void addFile(String path, File file) {
