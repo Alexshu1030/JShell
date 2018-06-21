ee33a24b14f12f2fbda101acb3785bef8ac0d139
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-21 00:55:43 +0000

    Created skeleton code for Path class;

---
+++ Assignment2/src/Path.java
@@ -0,0 +1,23 @@
+
+public class Path {
+  
+  public static String getRootDirectory(String path) {
+    
+    return "";
+  }
+  
+  public static String getFileName(String path) {
+        
+    return "";
+  }
+  
+  public static String getFileExtension(String path) {
+
+    return "";
+  }
+  
+  public static boolean isAbsolute(String path) {
+    
+    return false;
+  }
+}
