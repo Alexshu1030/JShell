bb2d662d378afc02d46e2a0fe8a5e5b2c7e62700
wuaster@UTORONTO.CA <wuaster@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-25 21:56:27 +0000

    Finished the man class

--- Assignment2/src/filesystem/Man.java
+++ Assignment2/src/filesystem/Man.java
@@ -162,28 +162,213 @@ public class Man {
           + " JShell will ignore excess spaces.");
     }
     else if (cmd = "pushd") {
-      
+      System.out.println("NAME:");
+      System.out.println("  pushd DIR - Pushes the current working directory"
+          + " into a stack, then change the current working directory"
+          + " to DIR.");
+      System.out.println("DESCRIPTION:");
+      System.out.println("  Saves the current working directory into a "
+          + "stack of directories, then changes the current working "
+          + "directory to DIR so that the old working directory could be"
+          + "returned. The directory stack is dynamic and changes "
+          + "depending on pushd and popd commands.");
+      System.out.println("PARAMETERS:");
+      System.out.println("  DIR - The new current working directory that the"
+          + " user may eventually want to return to through the directory"
+          + " stack.");
+      System.out.println("RETURNS:");
+      System.out.println("  This command does not return anything.");
+      System.out.println("EXAMPLE USAGE:");
+      System.out.println("  /#: pushd Dir1");
+      System.out.println("    will add the current working directory to"
+          + " the stack and change the current working directory to Dir1.");
     }
     else if (cmd = "popd") {
-      
+      System.out.println("NAME:");
+      System.out.println("  popd - Sets the current working directory "
+          + "to the one at the top of the directory stack.");
+      System.out.println("DESCRIPTION:");
+      System.out.println("  Removes the directory at the top of the "
+          + "directory stack and sets the current working directory to "
+          + "that directory.");
+      System.out.println("PARAMETERS:");
+      System.out.println("  There are no parameters available for this"
+          + " command.");
+      System.out.println("RETURNS:");
+      System.out.println("  Will return an error message if the "
+          + "directory stack is empty.");
+      System.out.println("EXAMPLE USAGE:");
+      System.out.println("If the directory stack is not empty.")
+      System.out.println("  /#: popd");
+      System.out.println("    will change the current working directory to "
+          + "the one at the top of the stack.");
+      System.out.println("If the directory stack is empty.");
+      System.out.println("  /#: popd");
+      System.out.println("    Will return an error message.")
     }
     else if (cmd = "history") {
-      
+      System.out.println("NAME:");
+      System.out.println("  history [number] - Prints recent commands"
+          + " entered");
+      System.out.println("DESCRIPTION:");
+      System.out.println("  Prints recent commands with each command on "
+          + "their own line. There will be two columns. The first column "
+          + "numbers the commands where the most recent is the highest"
+          + " number. The second column contains the commands.");
+      System.out.println("PARAMETERS:");
+      System.out.println("  number - This parameter lets the user return "
+          + "the last 'number' commands entered rather than all commands."
+          + " An optional parameter.");
+      System.out.println("RETURNS:");
+      System.out.println("  Will return a list of commands entered.");
+      System.out.println("EXAMPLE USAGE:");
+      System.out.println("If no other command was entered.")
+      System.out.println("  /#: history");
+      System.out.println("    1. history");
+      System.out.println("If we were to enter the pwd command first.");
+      System.out.println("  /#: history");
+      System.out.println("    1. pwd");
+      System.out.println("    2. history");
+      System.out.println("If we were to enter the pwd command twice first.");
+      System.out.println("  /#: history 2");
+      System.out.println("    1. pwd");
+      System.out.println("    2. history");
     }
     else if (cmd = "cat") {
-      
+      System.out.println("NAME:");
+      System.out.println("  cat FILE1 [FILE2 ...] - Displays the contents"
+          + " in the given file(s)");
+      System.out.println("DESCRIPTION:");
+      System.out.println("  Displays the contents of the file and "
+          + "concatenates the contents of subsequent files to the contents"
+          + " of the first.");
+      System.out.println("PARAMETERS:");
+      System.out.println("  FILE1 - The file with the contents to be"
+          + " displayed.");
+      System.out.println(("  FILE2 - A subsequent file with the contents "
+          + "to be displayed by concatenating it to FILE1. An optional"
+          + " parameter.");
+      System.out.println("RETURNS:");
+      System.out.println("  Will display the contents of the given files.");
+      System.out.println("EXAMPLE USAGE:");
+      System.out.println("If FILE1 contains 'hello'")
+      System.out.println("  /#: cat FILE1");
+      System.out.println("    will return 'hello'");
+      System.out.println("If FILE1 contains 'hello' and FILE2 contains "
+          + "'there'");
+      System.out.println("  /#: cat FILE1 FILE2");
+      System.out.println("    Will return 'hellothere'.")
     }
     else if (cmd = "echo") {
-      
+      System.out.println("NAME:");
+      System.out.println("  echo STRING [> OUTFILE |>> OUTFILE] - "
+          + "Print or add STRING to standard output");
+      System.out.println("DESCRIPTION:");
+      System.out.println("  Prints STRING to the JShell if STRING is the"
+          + " only parameter provided. If OUTFILE is provided, STRING will"
+          + " overwrite whatever is in OUTFILE if > is used, or append "
+          + "to OUTFILE if >> is used. If OUTFILE does not exist, it "
+          + "will be created.");
+      System.out.println("PARAMETERS:");
+      System.out.println("  STRING - The string that will be sent to"
+          + " standard output. Must be surrounded by double quotes");
+      System.out.println("  >  - Overwrite OUTFILE's contents with STRING. "
+          + "Optional parameter.");
+      System.out.println("  >>  - Append OUTFILE's contents with STRING. "
+          + "Optional parameter.");
+      System.out.println("  OUTFILE  - The file that STRING will output to"
+          + "depending on whether > or >> is used. OUTFILE will be created"
+          + " if it does not already exist. Optional parameter");
+      System.out.println("RETURNS:");
+      System.out.println("  Will print STRING if STRING is the only "
+          + "parameter.");
+      System.out.println("EXAMPLE USAGE:");
+      System.out.println("  /#: echo \"Hello\"").
+      System.out.println("    will display Hello");
+      System.out.println("If FILE1 contains 'hello'");
+      System.out.println("  /#: echo \"Hello\" > FILE1");
+      System.out.println("    FILE1 will contain Hello");
+      System.out.println("If FILE1 contains 'hello'");
+      System.out.println("  /#: echo \"Hello\" >> FILE1");
+      System.out.println("    FILE1 will contain helloHello.")
     }
     else if (cmd = "man") {
-      
+      System.out.println("NAME:");
+      System.out.println("  man CMD - Displays documentation for CMD ")
+      System.out.println("DESCRIPTION:");
+      System.out.println("  Displays name, description, possible parameters"
+          + " possible return messages, and example usage of cmd");
+      System.out.println("PARAMETERS:");
+      System.out.println("  CMD - The command for which the documentation "
+          + "will be displayed.");
+      System.out.println("RETURNS:");
+      System.out.println("  Documentation for CMD");
+      System.out.println("EXAMPLE USAGE:");
+      System.out.println("  /#: man man");
+      System.out.println("    will display documentation for man.");
+      System.out.println("  /#: man nam");
+      System.out.println("    Will return an error message because nam"
+          + " is not a valid command.")
     }
     else if (cmd = "find") {
-      
+      System.out.println("NAME:");
+      System.out.println("  find PATH ... -type [f|d] -name \"STRING\""
+          + " - Displays all files or directories with name STRING in PATH.")
+      System.out.println("DESCRIPTION:");
+      System.out.println("  If -type f then it will find all files with "
+          + "name STRING in PATH and display it. If type -d then it will"
+          + " find all directories with name STRING in PATH.");
+      System.out.println("PARAMETERS:");
+      System.out.println("  PATH - The path to find STRING in.");
+      System.out.println("  STRING - The name of the file/directory "
+          + "the user is looking for");
+      System.out.println("  -type [f|d] - f will find files named STRING "
+          + "in PATH, d will find directories named STRING in PATH");
+      System.out.println(" -name - parameter to specify STRING as the "
+          + "name of the file tehey are looking for.");
+      System.out.println("RETURNS:");
+      System.out.println("  A list of all files/directories in PATH named "
+          + "STRING");
+      System.out.println("EXAMPLE USAGE:");
+      System.out.println("  /#: find /user/Desktop -type d -name \"a\"");
+      System.out.println("    will display all directories in Desktop "
+          + "named 'a'.");
+      System.out.println("  /#: find /user/Desktop -type f -name \"b\"");
+      System.out.println("    will display all directories in Desktop \"\n" + 
+          "named \"b\".");
     }
     else if (cmd = "tree") {
-      
+      System.out.println("NAME:");
+      System.out.println("  tree - Displays the entire file system as a "
+          + "tree");
+      System.out.println("DESCRIPTION:");
+      System.out.println("Displays the entire file system as a "
+          + "tree starting from the root directory. For each level of "
+          + "the tree, the subtree is indented");
+      System.out.println("PARAMETERS:");
+      System.out.println("  There are no parameters available for this"
+          + " command.");
+      System.out.println("RETURNS:");
+      System.out.println("  A text representation of the file system");
+      System.out.println("EXAMPLE USAGE:");
+      System.out.println("If the root directory contains no subdirectories");
+      System.out.println("  /#: tree");
+      System.out.println("    will return /");
+      System.out.println("If the root directory contains 2 subdirectories A"
+          + " and B.");
+      System.out.println("  /#: tree");
+      System.out.println("   will return: ")
+      System.out.println("   /");
+      System.out.println("      A");
+      System.out.println("      B");
+      System.out.println("If the root directory contains 2 subdirectories A"
+          + " and B with a sub directory C in A");
+      System.out.println("  /#: tree");
+      System.out.println("   will return: ")
+      System.out.println("   /");
+      System.out.println("      A");
+      System.out.println("          C");
+      System.out.println("      B");
     }
   }
   
