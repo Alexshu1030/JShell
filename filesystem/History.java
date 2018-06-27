92e7d4d55e9afd2863a65ad0df52ea64ead31d1a
wuaster <wuaster@UTORONTO.CA>
2018-06-27 20:19:20 +0000

    Added history class

---
+++ filesystem/History.java
@@ -0,0 +1,21 @@
+package filesystem;
+public class History {
+  
+  public History() {
+    ArrayList<String> commands = new ArrayList<String>();
+  }
+  public void addCommand(String command) {
+    commands.add(0, command);
+  }
+  public void outputHistory(int n) {
+    for (int i = n; i<=commands.size(); i++) {
+      
+      System.out.println(i + ". " + commands[i-1]);
+    }
+  }
+  public void outputHistory() {
+    for (int i = 1; i <= commands.size(); i++) {
+      System.out.println(i + ". " + commands[i-1]);
+    }
+  }
+}
