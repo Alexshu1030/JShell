71dc5147a76403182fc343b3e608872ef3b6efd3
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-28 18:15:01 +0000

    Implemented Command interface methods.

--- Assignment2/src/commands/Mkdir.java
+++ Assignment2/src/commands/Mkdir.java
@@ -1,11 +1,48 @@
 package commands;
 import java.util.ArrayList;
 import filesystem.*;
-public class Mkdir {
-  public Mkdir(ArrayList<String> folders) {
-    Directory current = FileExplorer.getWorkingDirectory();
-    for (int i = 0; i < folders.size(); i++) {
-      Directory f = new Directory(folders.get(i), FileExplorer.getWorkingDirectory());
+import shell.JShellWindow;
+public class Mkdir implements Command {
+
+  private String commandName = "mkdir";
+  private String helpText = "Not finished...";
+  
+  public boolean Run(JShellWindow jShell, ArrayList<String> arguments) {
+    
+    FileExplorer explorer = jShell.GetFileExplorer();
+    
+    String path = arguments.get(0);
+    Directory parentDir = explorer.getDirectory(path);
+    System.out.println(parentDir.getFileName());
+    String dirName = Path.getFileName(path);
+    
+    Directory newDir = new Directory(dirName, parentDir);
+    parentDir.addFile(newDir);
+    
+    return true;
+  }
+
+  public String GetCommandName() {
+    
+    return commandName;
+  }
+
+  public boolean IsValidCommand(String commandName,
+      ArrayList<String> arguments) {
+    
+    boolean isValid = false;
+    
+    if (this.commandName.equals(commandName)) {
+      if (arguments.size() == 1) {
+        isValid = true;
+      }
     }
+    
+    return isValid;
+  }
+
+  public String GetHelpText() {
+   
+    return helpText;
   }
 }
