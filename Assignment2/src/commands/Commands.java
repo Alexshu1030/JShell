41e9b2c4281748269734d836be31e380bc3150e0
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-28 04:49:47 +0000

    Implemented methods.

--- Assignment2/src/commands/Commands.java
+++ Assignment2/src/commands/Commands.java
@@ -1,23 +1,120 @@
 package commands;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import driver.JShell;
+
 public class Commands {
 
+  private static Command[] commands = new Command[] {new Pwd()};
+  
+  public static void Run(JShell jShell, String command) {
+    
+    ArrayList<String> arguments = new ArrayList<String>(Arrays.asList(
+        Split(command)));
+    
+    int i = 0;
+    boolean found = false;
+   
+    while (i < commands.length && !found) {
+      if (commands[i].IsValidCommand(arguments)) {
+        commands[i].Run(jShell, arguments);
+        found = true;
+      }
+      else
+        i++;
+    }
+  }
+  
   public static boolean IsValidCommand(String command) {
     
-    // Split the command by whitespace
-    String[] splitCommand;
+    ArrayList<String> arguments = new ArrayList<String>(Arrays.asList(
+        Split(command)));
     
+    int i = 0;
+    boolean isValid = false;
+   
+    while (i < commands.length && !isValid) {
+      if (commands[i].IsValidCommand(arguments))
+        isValid = true;
+      else
+        i++;
+    }
     
-    return CommandExists(splitCommand[0]) && CommandArgumentsAreValid(command);
+    return isValid;
   }
-  
-  private static boolean CommandExists(String commandName) {
-    
-    return false;
-  }
-  
-  private static boolean CommandArgumentsAreValid(String command) {
-    
-    return false;
+  private static String[] Split(String text) {
+
+    boolean inQuotes = false;
+    boolean atTextEnd = false;
+
+    int splits = 0;
+
+    for (int i = 0; i < text.length(); i++) {
+
+      if (text.charAt(i) == '"') {
+
+        if (!inQuotes && atTextEnd)
+          atTextEnd = false;
+
+        inQuotes = !inQuotes;
+      }
+
+      if (!inQuotes) {
+        if (atTextEnd) {
+          if (text.charAt(i) != ' ')
+            atTextEnd = false;
+        }
+        if (text.charAt(i) == ' ' && !atTextEnd || i == text.length() - 1) {
+          atTextEnd = true;
+          splits++;
+        }
+      }
+    }
+
+
+    inQuotes = false;
+    atTextEnd = false;
+
+    int currentSplit = 0;
+    String[] splitText = new String[splits];
+
+    int beginIndex = 0;
+    int numQuotes = text.length() - text.replace("\"", "").length();
+    if (numQuotes%2 == 0) {
+
+      for (int i = 0; i < text.length(); i++) {
+
+        if (text.charAt(i) == '"') {
+
+          if (!inQuotes) {
+            atTextEnd = false;
+            beginIndex = i;
+          }
+          inQuotes = !inQuotes;
+        }
+
+        if (!inQuotes) {
+          if (atTextEnd) {
+            if (text.charAt(i) != ' ') {
+              beginIndex = i;
+              atTextEnd = false;
+            }
+          }
+          else {
+            if (text.charAt(i) == ' ') {
+              atTextEnd = true;
+              splitText[currentSplit] = text.substring(beginIndex, i);
+              currentSplit++;
+            }
+          }
+
+        }
+        if (i == text.length() - 1) {
+          splitText[currentSplit] = text.substring(beginIndex);
+        }
+      }
+    }
+    return splitText;
   }
 }
