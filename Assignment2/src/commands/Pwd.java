bd3d7b35b2cf4c234184a2395d557d054c7e4990
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-28 04:30:21 +0000

    Implemented the interface methods.

--- Assignment2/src/commands/Pwd.java
+++ Assignment2/src/commands/Pwd.java
@@ -1,5 +1,56 @@
 package commands;
 
-public class Pwd {
+import java.util.ArrayList;
+import driver.JShell;
+import filesystem.FileExplorer;
 
+public class Pwd implements Command {
+
+  private String commandName = "pwd";
+  private int numOfArguments = 1;
+  private String helpText = "NAME:\n"
+      + "  pwd - Prints current working directory\n"
+      + "DESCRIPTION:\n"
+      + "  Prints the full path of the current working directory.\n"
+      + "PARAMETERS:\n"
+      + "  There are no parameters available for"
+      + " this command.\n"
+      + "RETURNS:\n"
+      + "  This command does not return anything.\n"
+      + "EXAMPLE USAGE:\n"
+      + "  /#: exit\n"
+      + "    will successfully terminate JShell.\n"
+      + "  /#: exit p\n"
+      + "    will not terminate JShell because of the "
+      + " additional parameter 'p'.\n"
+      + "  /#:              exit\n"
+      + "    will terminate JShell because\n"
+      + " JShell will ignore excess spaces.\n";
+  
+  public boolean IsValidCommand(ArrayList<String> arguments) {
+    
+    boolean isValid = false;
+    
+    if (arguments.size() == numOfArguments) {
+      if (arguments.get(0).equals(commandName)) {
+        isValid = true;
+      }
+    }
+    
+    return isValid;
+  }
+
+  public String GetHelpText() {
+    
+    return helpText;
+  }
+
+  public void Run(JShell jShell, ArrayList<String> arguments) {
+    
+    FileExplorer fileExplorer = jShell.GetFileExplorer();
+    
+    String workingDirPath = fileExplorer.getWorkingDirectory().GetFullPath();
+    
+    System.out.println(workingDirPath);
+  }
 }
