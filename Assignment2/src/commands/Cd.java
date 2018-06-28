3585fa9b8fbd1b9c25b653cdb843e33c766a0496
wuaster@UTORONTO.CA <wuaster@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-28 17:29:33 +0000

    Implemented interface commands for cd

--- Assignment2/src/commands/Cd.java
+++ Assignment2/src/commands/Cd.java
@@ -4,8 +4,13 @@ import filesystem.*;
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Cd {
+public class Cd implements Command {
+  private int numOfArguments = 0;
+  private String commandName = "cd";
   public Cd (String path) {
+   
+  }
+  public boolean Run(JShellWindow jShell, ArrayList<String> arguments) {
     Directory currentDirectory = FileExplorer.getWorkingDirectory();
     if (path == "..") {
       FileExplorer.setWorkingDirectory(currentDirectory.getFileDirectory());
@@ -32,5 +37,23 @@ public class Cd {
         }
       }
     }
+  }
+  
+  
+  public String GetCommandName() {
+    return commandName;
+  }
+  
+  public boolean IsValidCommand(String commandName, 
+      ArrayList<String> arguments) {
+    boolean isValid = false;
+    if (arguments.size() == numOfArguments) {
+      isValid = true;
+    }
+    return isValid;
+  }
+  
+  public String GetHelpText() {
+    Man.manCd();
   }
 }
