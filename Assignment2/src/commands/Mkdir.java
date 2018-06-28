80f4aa5a982bd3f8bd84cf0bdd2068d4d6857908
huan1085@UTORONTO.CA <huan1085@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-28 03:46:47 +0000

    changed cd and mkdir

--- Assignment2/src/commands/Mkdir.java
+++ Assignment2/src/commands/Mkdir.java
@@ -2,7 +2,7 @@ package commands;
 import java.util.ArrayList;
 import filesystem.*;
 public class Mkdir {
-  public Mkdir(ArrayList<filesystem.Directory> folders) {
+  public Mkdir(ArrayList<String> folders) {
     for (int i = 0; i < folders.size(); i++) {
       Directory f = new Directory(folders.get(i), pwd());
     }
