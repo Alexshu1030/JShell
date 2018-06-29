283a64e901d4c28ce5a9012060028acd6d705caf
wuaster@UTORONTO.CA <wuaster@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-29 17:24:02 +0000

    Created base echo class with comments on how to finish it.

---
+++ Assignment2/src/commands/Echo.java
@@ -0,0 +1,47 @@
+package commands;
+
+import java.util.ArrayList;
+import shell.JShellWindow;
+import filesystem.FileExplorer;
+import filesystem.File;
+public class Echo {
+  private String command = "echo";
+  public boolean Run(JShellWindow jShell, ArrayList<String> arguments) {
+    if (arguments.size() == 1) {
+      System.out.println(arguments.get(0));
+    }
+    else if (arguments.size() == 2) {
+      if (arguments.get(1) == ">" || arguments.get(1) == ">>") {
+        System.out.println(arguments.get(0));
+      }
+    }
+    else if (arguments.size() == 3) {
+      if (arguments.get(1) == ">") {
+       // We need to see if the file/argument3 exists in the current folder then 
+       // set its contents to the first argument i.e. arguments.get(0)
+      }
+      else if (arguments.get(1) == ">>") {
+     // We need to see if the file/argument3 exists in the current folder then
+     // put the current contents in a string and append a line separator to
+     // it (which is a requirement) then append argument1 to the string.
+     // Finally set the contents of the file to be the string
+      }
+    }
+    return true;
+  }
+  
+  public String GetCommandName() {
+    return command;
+  }
+  
+  public boolean IsValidCommand(String commandName,
+      ArrayList<String> arguments) {
+    return (commandName == command && arguments.size() <= 3 && 
+        arguments.size() >= 1)
+  }
+    
+  
+  public void GetHelpText() {
+    Man.manEcho();
+  }
+}
