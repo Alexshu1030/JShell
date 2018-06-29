3f08e478326d4e4ce28f738ed175f0e26c341e71
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-29 22:50:32 +0000

    Added comments.

--- Assignment2/src/filesystem/FileExplorer.java
+++ Assignment2/src/filesystem/FileExplorer.java
@@ -31,8 +31,12 @@ package filesystem;
 
 public class FileExplorer {
 
+  // This is static so multiple instances of the FileExplorer will have
+  // access to the same directory.
   private static Directory rootDirectory = new Directory("", null);
   
+  // This is not static because each FileExplorer should be able to have a
+  // different working directory.
   private Directory workingDirectory;
   
   public FileExplorer() {
@@ -55,27 +59,35 @@ public class FileExplorer {
     workingDirectory = newWD;
   }
   
-  // Returns the file at the given path
+  // Returns the file at the given path.
   public File getFile(String path) {
     
-    Directory dir = getDirectory(path);
+    // Get the directory the file is in
+    Directory dir = getParentDirectory(path);
+    // Get the name of the file and get it from the directory.
     String fileName = Path.getFileName(path);
     File file = dir.getFile(fileName);
     
     return file;
   }
   
-  public Directory getDirectory(String path) {
+  public Directory getParentDirectory(String path) {
     
+    // Get the path of the directory that the file is in
     String dirPath = Path.removeFileName(path);
 
     Directory rootDir;
     
     if (Path.isAbsolute(path))
+      // The path is absolute so we want to start at the root directory and
+      // work our way to the file.
       rootDir = rootDirectory;
     else
+      // The path is relative so we want to start in the workign directory
+      // and work our way to the file.
       rootDir = workingDirectory;
     
+    // Use the helper method.
     return getDirectoryHelper(rootDir, dirPath);
   }
   
@@ -84,18 +96,24 @@ public class FileExplorer {
     Directory dir;
     
     if (relPath.equals("")) {
+      // Base case. We are at the end of the relative path. The current
+      // The current directory that we are in is the one we are looking for.
       dir = curDir;
     }
     else {
       
+      // Get the next directory we want to look in
       String nextDirName = Path.getRootDirectory(relPath);
       Directory nextDir = (Directory)curDir.getFile(nextDirName);
       
       if (nextDir != null) {
+        // We now want to get the path relative to this directory. And then
+        // run the method again on that path with that directory.
         String newRelPath = Path.getSubPath(relPath);
         dir = getDirectoryHelper(nextDir, newRelPath);
       }
       else {
+        // The path doesn't exit. Return null.
         dir = null;
       }
     }
@@ -107,13 +125,16 @@ public class FileExplorer {
   // Adds the file at the given path
   public void addFile(String path, File file) {
     
-    Directory dir = getDirectory(path);
+    // Get the parent directory path and then add the file into that
+    // directory.
+    Directory dir = getParentDirectory(path);
     dir.addFile(file);
   }
   
   // Returns whether that path exists in the file explorer
   public boolean pathExists(String path) {
     
+    //Get the file and check that it isn't null.
     File file = getFile(path);
     
     return file != null;
