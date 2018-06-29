109acf401494dccb4b0010f55a4d65cdc66492e8
wuaster <wuaster@UTORONTO.CA>
2018-06-29 17:33:38 +0000

    Added base class with instructions on how to finish

---
+++ commands/Cat.java
@@ -0,0 +1,30 @@
+package commands;
+
+import java.util.ArrayList;
+import shell.JShellWindow;
+
+public class Cat {
+  private String command = "cat";
+  public boolean Run(JShellWindow jShell, ArrayList<String> arguments) {
+    for (int i = 0; i < arguments.size(); i++) {
+      // We need to replace the argument at i with a File object
+      System.out.println(arguments.get(i).getFileContents());
+      // make sure we are not at the last file
+      if (i != arguments.size()-1) {
+        System.out.print("\n\n\n");
+      }
+    }
+  }
+  
+  public String GetCommandName() {
+    return command;
+  }
+  public boolean IsValidCommand(String commandName,
+      ArrayList<String> arguments) {
+    return (command == commandName && arguments.size() >= 1);
+  }
+  
+  public void GetHelpText() {
+    Man.manCat();
+  }
+}
