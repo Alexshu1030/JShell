43694cc88dbcb3e9a604c2fb15512f827e4824d3
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-29 22:33:16 +0000

    Resolved conflict and manually merged the files.

--- Assignment2/src/commands/Commands.java
+++ Assignment2/src/commands/Commands.java
@@ -1,94 +1,65 @@
-// **********************************************************
-// Assignment2:
-// Student1: Ciaran Hogan
-// UTORID user_name: hogancia
-// UT Student #: 1003796859
-// Author: Ciaran Hogan
-//
-// Student2: Jeremy Tanuan
-// UTORID user_name: tanuanje
-// UT Student #: 1003939982
-// Author: Jeremy Tanuan
-//
-// Student3: Aster Wu
-// UTORID user_name: wuaster
-// UT Student #: 1004175117
-// Author: Aster Wu
-//
-// Student4: Yan Chen Huang
-// UTORID user_name: huan1085
-// UT Student #: 1004325857
-// Author: Yan Chen Huang
-//
-//
-// Honor Code: I pledge that this program represents my own
-// program code and that I have coded on my own. I received
-// help from no one in designing and debugging my program.
-// I have also read the plagiarism section in the course info
-// sheet of CSC B07 and understand the consequences.
-// *********************************************************
 package commands;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import driver.JShell;
 import shell.JShellWindow;
 
 public class Commands {
 
+  // This is a list of the valid commands
   private static Command[] commands = new Command[] {new Exit(), new Pwd(), new Ls(), 
       new Mkdir(), new Cd()};
   
-  public static boolean Run(JShellWindow jShell, String command) {
+  public static boolean Run(JShellWindow jShell, String commandText) {
     
+    // Split the command into it's parts (i.e. separate blocks of text, quotes
+    // and etc. )
     ArrayList<String> arguments = new ArrayList<String>(Arrays.asList(
-        Split(command)));
+        Split(commandText)));
     
     boolean succeeded = false;
     
+    // There must be at least one element in the arguments for it to be a
+    // valid command
     if (arguments.size() > 0) {
       
+      // The command name must be the first element
       String commandName = arguments.remove(0);
+      Command command = GetCommand(commandName);
       
-      int i = 0;
-      boolean found = false;
-     
-      while (i < commands.length && !found) {
-        if (commands[i].IsValidCommand(commandName, arguments)) {
-          succeeded = commands[i].Run(jShell, arguments);
-          found = true;
+      if (command != null) {
+        if (command.AreValidArguments(arguments)) {
+          // We have found the command and it's arguments are valid. We can
+          // now run the command
+          succeeded = command.Run(jShell, arguments);
         }
-        else
-          i++;
       }
     }
     
     return succeeded;
   }
   
-  public static boolean IsValidCommand(String command) {
+  public static Command GetCommand(String name) {
     
-    ArrayList<String> arguments = new ArrayList<String>(Arrays.asList(
-        Split(command)));
-    
-    boolean isValid = false;
-    
-    if (arguments.size() > 0) {
-      
-      String commandName = arguments.remove(0);
-    
-      int i = 0;
-     
-      while (i < commands.length && !isValid) {
-        if (commands[i].IsValidCommand(commandName, arguments))
-          isValid = true;
-        else
-          i++;
+    int i = 0;
+    boolean found = false;
+    Command command = null;
+   
+    // Iterate over the commands until the correct one has been found
+    while (i < commands.length && !found) {
+      // Check if the name of the current command is the one we want
+      if (commands[i].GetCommandName().equals(name)) {
+        // It is. We can now exit the loop
+        command = commands[i];
+        found = true;
       }
+      else
+        i++; // It isn't. Go to the next command
     }
     
-    return isValid;
+    return command;
   }
+  
   private static String[] Split(String text) {
 
     boolean inQuotes = false;
