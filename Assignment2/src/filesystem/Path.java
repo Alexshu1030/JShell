dafcced20fb997c1487831202e5fdd191ab3b671
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-29 23:00:37 +0000

    Added comments.

--- Assignment2/src/filesystem/Path.java
+++ Assignment2/src/filesystem/Path.java
@@ -33,10 +33,13 @@ public class Path {
   
   public static String getSubPath(String path) {
     
+    // Get the first slash in the path.
     int firstSlash = path.indexOf('/');
     
     String result = "";
     
+    // If there is a first slash, then return everything after it. Otherwise
+    // this is a folder or file and there is no subpath.
     if (firstSlash != -1)
       result = path.substring(firstSlash + 1);
     
@@ -49,14 +52,21 @@ public class Path {
     // (which is a slash)
     if (isAbsolute(path))
       path = path.substring(1);
-      
+     
+    
+    // Get the first slash in the path
     int firstSlash = path.indexOf('/');
     
     String result = "";
     
     if (firstSlash != -1)
+      // Return everything up to the slash. This is the root directory of the
+      // path.
       result = path.substring(0, firstSlash);
     else {
+      // Since there is no slash in the path, this must be a file or folder
+      // name. If it is a folder then we want to return it. Otherwise we just
+      // want to return the root directory.
       if (getFileExtension(path) == "")
         result = path;
     }
@@ -66,11 +76,15 @@ public class Path {
   
   public static boolean isDirectory(String path) {
     
+    // If the file has no extension then it is a folder
     return getFileExtension(path).equals("");
   }
   
   public static String getFileName(String path) {
-        
+       
+    // Get the last slash in the path. The name starts at the next index. Also
+    // if there is no slash and this is just the name of a file or folder,
+    // then this will be zero and it will return the path unaltered, as wanted.
     int nameStartIndex = path.lastIndexOf('/') + 1;
     
     return path.substring(nameStartIndex);
@@ -78,10 +92,13 @@ public class Path {
   
   public static String getFileExtension(String path) {
 
+    // Get the last decimal in the path.
     int decimalIndex = path.lastIndexOf('.');
     
     String result = "";
     
+    // We want to return all of the text after the decimal. If there is no
+    // decimal, then it is a folder and we want to return an empty string.
     if (decimalIndex != -1)
       result = path.substring(decimalIndex);
       
@@ -90,17 +107,30 @@ public class Path {
   
   public static boolean isAbsolute(String path) {
     
-    // Get the first character in the path
-    char firstChar = path.charAt(0);
+    boolean isAbs = false;
+    
+    if (path != "") {
+      // Get the first character in the path
+      char firstChar = path.charAt(0);
+      // If the first character is a slash then it is an absolute path.
+      isAbs = firstChar == '/';
+    }
    
-    // If the first character is a slash then it is an absolute path.
-    return firstChar == '/';
+    return isAbs;
   }
   
   public static String removeFileName(String path) {
     
+    // Get the last slash in the path
     int nameStartIndex = path.lastIndexOf('/');
     
-    return path.substring(0, nameStartIndex);
+    String pathWithoutFileName = "";
+    
+    // If there is no slash in the path. Then this is the name of a file or
+    // folder. Therefore we need to return an empty string.
+    if (nameStartIndex != -1)
+      pathWithoutFileName = path.substring(0, nameStartIndex);
+    
+    return pathWithoutFileName;
   }
 }
