dacf745147667d76d44765e29f975ed4eb0c55d4
tanuanje@UTORONTO.CA <tanuanje@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-29 23:02:03 +0000

    revised implementation

--- Assignment2/src/commands/History.java
+++ Assignment2/src/commands/History.java
@@ -3,28 +3,39 @@ import java.util.ArrayList;
 import shell.JShellWindow;
 
 public class History implements Command{
-  
+
   private String commandName = "history";
 
+  @Override
   public boolean Run(JShellWindow jShell, ArrayList<String> arguments) {
-
     ArrayList<String> log = jShell.getLog();
-    
-    return false;
+    int truncAmount;
+    if (arguments.isEmpty()) {
+      truncAmount = log.size();
+    } else {
+      truncAmount = (Integer.parseInt((arguments.get(0))));
+    }
+    for (int i = truncAmount; i >= 0; i--) {
+      System.out.println(truncAmount - i + ". " + log.get(i));
+    }
+    return true;
   }
 
+  @Override
   public String GetCommandName() {
 
     return commandName;
   }
 
+  @Override
   public boolean AreValidArguments(ArrayList<String> arguments) {
 
     boolean isValid = false;
-    if (arguments.size() == 1) {
+
+    if (arguments.isEmpty()) {
       isValid = true;
       // check if argument is an integer
-    } else if (arguments.size() == 2) {
+    } else if (arguments.size() == 1) {
       try {
         Integer.parseInt(arguments.get(1));
       } catch (Exception NumberFormatException) {
@@ -35,6 +46,7 @@ public class History implements Command{
     return isValid;
   }
 
+  @Override
   public String GetHelpText() {
 
     Man.manHistory();
