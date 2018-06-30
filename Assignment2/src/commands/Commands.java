14df3ea69d9a91158c894b4aaee59bf6566c9a36
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-06-30 03:44:11 +0000

    Added honor code.

--- Assignment2/src/commands/Commands.java
+++ Assignment2/src/commands/Commands.java
@@ -1,3 +1,32 @@
+// **********************************************************
+// Assignment2:
+// Student1: Ciaran Hogan
+// UTORID user_name: hogancia
+// UT Student #: 1003796859
+// Author: Ciaran Hogan
+//
+// Student2: Jeremy Tanuan
+// UTORID user_name: tanuanje
+// UT Student #: 1003939982
+// Author: Jeremy Tanuan
+//
+// Student3: Aster Wu
+// UTORID user_name: wuaster
+// UT Student #: 1004175117
+// Author: Aster Wu
+//
+// Student4: Yan Chen Huang
+// UTORID user_name: huan1085
+// UT Student #: 1004325857
+// Author: Yan Chen Huang
+//
+//
+// Honor Code: I pledge that this program represents my own
+// program code and that I have coded on my own. I received
+// help from no one in designing and debugging my program.
+// I have also read the plagiarism section in the course info
+// sheet of CSC B07 and understand the consequences.
+// *********************************************************
 package commands;
 
 import java.util.ArrayList;
@@ -8,7 +37,8 @@ public class Commands {
 
   // This is a list of the valid commands
   private static Command[] commands = new Command[] {new History(), new Exit(),
-      new Pwd(), new Ls(), new Mkdir(), new Cd(), new Tree()};
+      new Pwd(), new Ls(), new Mkdir(), new Cd(), new Tree(), new Echo(),
+      new Find(), new Cat()};
   
   public static boolean run(JShellWindow jShell, String commandText) {
     
