153eef6a62514405128c21bdd790d0d1c1993eac
tanuanje <tanuanje@UTORONTO.CA>
2018-06-30 00:16:26 +0000

    added documentation of Exit

--- commands/Exit.java
+++ commands/Exit.java
@@ -7,23 +7,43 @@ public class Exit implements Command{
 
   private String commandName = "exit";
 
+
+  /**
+   * Returns true if successfully terminated the jShell
+   * @param jShell this is the window that will be printed on
+   * @param arguments this is a list containing null
+   * @return true this is true if execution was successful, o/w false
+   */
   public boolean Run(JShellWindow jShell, ArrayList<String> arguments) {
     jShell.setTerminate(true);
     return true;
   }
 
+
+  /**
+   * Returns the name of this command, "exit"
+   * @return commandName this is the command's name
+   */
   public String GetCommandName() {
     return commandName;
   }
 
+
+  /**
+   * Returns true if arguments are valid for "exit", o/w false
+   * @param arguments this is a list containing the arguments input
+   * @return isValid this is true if arguments are valid, o/w false
+   */
   public boolean AreValidArguments(ArrayList<String> arguments) {
-    
-    return arguments.isEmpty();
+    boolean isValid = arguments.isEmpty();
+    return isValid;
   }
 
+  /**
+   * Prints help text that includes documentation of Exit
+   */
   public String GetHelpText() {
     Man.manExit();
     return "";
   }
-
 }
