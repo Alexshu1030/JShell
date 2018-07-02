4a3d6b5b8e5fe305169c1857882a85510baa2f8b
hogancia@UTORONTO.CA <hogancia@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-07-02 01:40:30 +0000

    Removed magic string.

--- Assignment2/src/driver/JShell.java
+++ Assignment2/src/driver/JShell.java
@@ -58,76 +58,10 @@ public class JShell {
       window.addInputtoLog(input);
       
       if (!Commands.run(window, input)) {
-        System.out.println("Invalid command");
+        System.out.println(errorMessage);
       }
     }
+    
     in.close();
   }
-
-  /*
-  private static boolean IsValidCommand(String[] input) {
-
-    boolean isCommand = false;
-    int inputLen = input.length;
-
-    // If there is no input then it can't be a command
-    if (inputLen > 0) {
-      // Check if the first split matches any of the commands
-      for (int i = 0; i < commandNames.length; i++) {
-        if (commandNames[i].equals(input[0])) {
-          // A command has entered. Now we have to make sure that there are
-          // the correct number of arguments
-          if (commandArgs[i] == specificCase) {
-            // If command is mkdir or cat, then there must be 1 or more arguments
-            if ((i == 1 || i == 8 ) && inputLen >= 2) {
-              isCommand = true;
-              // if command is ls, it is always valid
-            } else if (i == 3) {
-              isCommand = true;
-              // if command is history, either no argument or integer argument
-            } else if (i == 7) {
-              // no argument
-              if (inputLen == 1) {
-                isCommand = true;
-                // check if argument is an integer
-              } else if (inputLen == 2) {
-                try {
-                  Integer.parseInt(input[1]);
-                } catch (Exception NumberFormatException) {
-                  return isCommand;
-                }
-                isCommand = true;
-              }
-              // if command is echo, it is valid if printing on shell
-            } else if (i == 9) {
-              if (inputLen == 2) {
-                int secArgLen = input[1].length();
-                // echo's STRING input must have quotations around it
-                if (input[1].substring(0, 1).equals("\"") &&
-                    input[1].substring(secArgLen - 1, secArgLen).equals("\"")) {
-                  isCommand = true;
-                }
-              } else if (inputLen == 4) {
-                // Otherwise the second argument must be the > or >>
-                int secArgLen = input[1].length();
-                if (input[2].equals(">>") || input[2].equals(">")) {
-                  // echo's STRING input must have quotations around it
-                  if (input[1].substring(0, 1).equals("\"") &&
-                      input[1].substring(secArgLen - 1, secArgLen).equals("\"")) {
-                    isCommand = true;
-                  }
-                }
-              }
-            }
-          } else if (commandArgs[i] == inputLen - 1) {
-            // The command name is correct and there are the right number of
-            // arguments. This is a command.
-            isCommand = true;
-          }
-        }
-      }
-    }
-    return isCommand;
-  }
-  */
 }
