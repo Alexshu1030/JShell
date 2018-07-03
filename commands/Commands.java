80faa20dc7e107d333182f7ba7fe85bd71124976
hogancia <hogancia@UTORONTO.CA>
2018-07-03 03:40:07 +0000

    Removed commented out code.

--- commands/Commands.java
+++ commands/Commands.java
@@ -166,47 +166,4 @@ public class Commands {
     
     return splits;
   }
-
-  /*
-   * private static String[] split(String text) {
-   * 
-   * boolean inQuotes = false; boolean atTextEnd = false;
-   * 
-   * int splits = 0;
-   * 
-   * for (int i = 0; i < text.length(); i++) {
-   * 
-   * if (text.charAt(i) == '"') {
-   * 
-   * if (!inQuotes && atTextEnd) atTextEnd = false;
-   * 
-   * inQuotes = !inQuotes; }
-   * 
-   * if (!inQuotes) { if (atTextEnd) { if (text.charAt(i) != ' ') atTextEnd =
-   * false; } if (text.charAt(i) == ' ' && !atTextEnd || i == text.length() - 1)
-   * { atTextEnd = true; splits++; } } }
-   * 
-   * 
-   * inQuotes = false; atTextEnd = false;
-   * 
-   * int currentSplit = 0; String[] splitText = new String[splits];
-   * 
-   * int beginIndex = 0; int numQuotes = text.length() - text.replace("\"",
-   * "").length(); if (numQuotes%2 == 0) {
-   * 
-   * for (int i = 0; i < text.length(); i++) {
-   * 
-   * if (text.charAt(i) == '"') {
-   * 
-   * if (!inQuotes) { atTextEnd = false; beginIndex = i; } inQuotes = !inQuotes;
-   * }
-   * 
-   * if (!inQuotes) { if (atTextEnd) { if (text.charAt(i) != ' ') { beginIndex =
-   * i; atTextEnd = false; } } else { if (text.charAt(i) == ' ') { atTextEnd =
-   * true; splitText[currentSplit] = text.substring(beginIndex, i);
-   * currentSplit++; } }
-   * 
-   * } if (i == text.length() - 1) { splitText[currentSplit] =
-   * text.substring(beginIndex); } } } return splitText; }
-   */
 }
