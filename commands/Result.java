d80528ad0c1e6ef638fe20447b41c0e43da6679b
hogancia <hogancia@UTORONTO.CA>
2018-07-17 00:49:02 +0000

    Implemented the Result class.

--- commands/Result.java
+++ commands/Result.java
@@ -1,5 +1,64 @@
 package commands;
 
-public class Result {
+import java.util.ArrayList;
 
+public class Result {
+  
+  private String message = "";
+  private String errorMessage = "";
+  private boolean errorOccured = false;
+
+  private ArrayList<String> arguments;
+ 
+  
+  public Result(ArrayList<String> arguments) {
+    
+    this.arguments = arguments;
+  }
+  
+  public String getMessage() {
+    
+    return message;
+  }
+  
+  public void addMessage(String message, String spacing) {
+    
+    if (this.message.equals(""))
+      this.message = message;
+    else
+      this.message += spacing + message;
+  }
+  
+  public boolean errorOccured() {
+    
+    return errorOccured;
+  }
+  
+  public void RegisterError(String errorMessage) {
+    
+    errorOccured = true;
+    
+    // If there is already an error registered then we want to add the new one
+    // to the next line.
+    if (this.errorMessage.equals(""))
+      this.errorMessage = errorMessage;
+    else
+      this.errorMessage += "\n" + errorMessage;
+  }
+  
+  public void RegisterError(int argument, String errorMessage) {
+    
+    errorOccured = true;
+    
+    // Add the argument to the end of the error so the user can see which on it
+    // applies to.
+    errorMessage += " (Argument " + argument + ": " +
+    arguments.get(argument) + ")";
+    // If there is already an error registered then we want to add the new one
+    // to the next line.
+    if (this.errorMessage.equals(""))
+      this.errorMessage = errorMessage;
+    else
+      this.errorMessage += "\n" + errorMessage;
+  }
 }
