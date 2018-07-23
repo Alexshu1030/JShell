b218ef0fe271bab97ec8d124b4a763221c367c0d
huan1085 <huan1085@UTORONTO.CA>
2018-07-23 03:43:58 +0000

    added grep

--- commands/Grep.java
+++ commands/Grep.java
@@ -33,6 +33,8 @@ package commands;
 import java.util.ArrayList;
 import commandsystem.Command;
 import commandsystem.Result;
+import filesystem.File;
+import Exceptions.FileNotFoundException;
 import shell.JShellWindow;
 
 public class Grep implements Command {
@@ -56,13 +58,12 @@ public class Grep implements Command {
       + "-R - The option to search recursively through a folder (scanning "
       + "all files).\n"
       + "REGEX - The expression that we are looking for in the PATH.\n"
-      + "PATH - The file or folder that we are searching from.\n" 
-      + "RETURNS:\n"
+      + "PATH - The file or folder that we are searching from.\n" + "RETURNS:\n"
       + "All lines containing REGEX inside PATH.\n" + "EXAMPLE USAGE:\n"
       + "grep -R a b\n"
       + "This will recursively return all lines containing a in all files "
-      + "inside b\n"
-      + "grep a b.txt\n" + "This will return all lines containing a in b.txt";
+      + "inside b\n" + "grep a b.txt\n"
+      + "This will return all lines containing a in b.txt";
 
   /**
    * Returns true if successfully terminated the jShell
@@ -72,13 +73,105 @@ public class Grep implements Command {
    * @return true the output to the shell
    */
   public Result run(JShellWindow jShell, ArrayList<String> arguments) {
-
+    // instantiate a new cat object
+    Cat checkFile = new Cat();
+    // check if arguments are valid
     Result result = areValidArguments(arguments);
+    // set a list of files to the arguments
+    ArrayList<String> files = arguments;
+    // if the arguments are valid
     if (!result.errorOccured()) {
+      // if the recursive arg is provided (recursively go thru folder
       if (arguments.get(0).equals("-R")) {
-
+        // remove the -R and the regex respectively
+        files.remove(0);      
+        files.remove(0);
+        // get the regex we're looking for
+        String regex = arguments.get(1);
+        // for each folder in the list
+        for (int i = 0; i < files.size(); i++) {
+          // try to go thru each file and add their matching lines to the msg
+          try {
+            // if each of the args is an directory
+            if (jShell.getFileExplorer().getFile(files.get(i)).isDirectory()) {
+              // set the current folder to be used with cat
+              ArrayList<String> currFolder = new ArrayList<String>();
+              currFolder.add(files.get(i));
+              // make a new ls object
+              Ls folderCont = new Ls();
+              // get all the file names in the folder in eachFile
+              String eachFile = folderCont.run(jShell, currFolder).getMessage();
+              // split the file names into an array
+              String filesInDir[] = eachFile.split("\\s+");
+              // for each file name in the array
+              for (int k = 0; k < filesInDir.length; k++) {
+                File currFile = jShell.getFileExplorer().getFile(filesInDir[k]);
+                // if the currfile is not a directory
+                if (!currFile.isDirectory()) {
+                  // run cat on the file
+                  ArrayList<String> getCat = new ArrayList<String>();
+                  getCat.add(filesInDir[k]);
+                  // split the contents by new lines
+                  String fileContents[] =
+                      checkFile.run(jShell, getCat).getMessage().split("\\n");
+                  // for each line in the file's cat
+                  for (int g = 0; g < fileContents.length; g++) {
+                    // add the line to result if it contains the regex
+                    if (fileContents[g].contains(regex)) {
+                      result.addMessage(fileContents[g]);
+                    }
+                  }
+                  // clear getcat
+                  getCat.clear();
+                } else {
+                  // if the currfile is a folder, call the method itself again
+                  ArrayList<String> recursiveFolder = new ArrayList<String>();
+                  recursiveFolder.add(currFile.getFileName());
+                  run(jShell, recursiveFolder);
+                }
+              }
+              // clear currfolder
+              currFolder.clear();
+            }
+            // catch exception and log error if the invalid path
+          } catch (FileNotFoundException e) {
+            result.logError(0, "The path does not exist.");
+          }
+        }
+      // if no -R is provided
+      } else {
+        // set regex to the first word in the argument
+        String regex = arguments.get(0);
+        // remove the regex so that files contains the file names
+        files.remove(0);
+        // for each file in files
+        for (int j = 0; j < files.size(); j++) {
+          // try to get each file
+          try {
+            if (!(jShell.getFileExplorer().getFile(files.get(j))
+                .isDirectory())) {
+              // set current file's name
+              ArrayList<String> currFile = new ArrayList<String>();
+              currFile.add(files.get(j));
+              // get the content of the file by running cat
+              String fileCont = checkFile.run(jShell, currFile).getMessage();
+              // split the content into separate lines
+              String fileLines[] = fileCont.split("\\n");
+              // if a line contains the regex add it to the result
+              for (int h = 0; h < fileLines.length; h++) {
+                if (fileLines[h].contains(regex)) {
+                  result.addMessage(fileLines[h]);
+                }
+              }
+            }
+            // catch error and log error if invalid path
+          } catch (FileNotFoundException e) {
+            result.logError(0, "The path does not exist.");
+          }
+        }
       }
     }
+    // return the result
     return result;
   }
 
