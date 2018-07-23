735806a3a062963dcf9f5c879fd2f4a588722c92
tanuanje <tanuanje@UTORONTO.CA>
2018-07-23 23:19:54 +0000

    added beforeclass and fixed instance of jShell and explorer

--- test/LsTest.java
+++ test/LsTest.java
@@ -1,35 +1,57 @@
 package test;
 
 import static org.junit.Assert.*;
-import org.junit.Before;
-import org.junit.Test;
-import commandsystem.Result;
-import commandsystem.Commands;
+import java.lang.reflect.Field;
+import org.junit.*;
+import commandsystem.*;
+import filesystem.*;
 import shell.JShellWindow;
 
 public class LsTest {
-  @Before
-  public void setup() {
+  private static JShellWindow jShell;
+  private static FileExplorer explorer;
+  
+  @BeforeClass
+  public static void setUpClass() {
     Commands.initializeCommandHashTable();
+  }
+  
+  @Before
+  public void setUp() {
+    jShell = new JShellWindow();
+    explorer = jShell.getFileExplorer();
+  }
+
+  @After
+  public void tearDown() throws NoSuchFieldException, SecurityException,
+  IllegalArgumentException, IllegalAccessException {
+    // Reset the static self reference in the FileExplorer class
+    Field field = (explorer.getClass()).getDeclaredField("rootDirectory");
+    field.setAccessible(true);
+    field.set(null, null);
+  }
+
+  @Test
+  public void testNoArguments() {
+    Result resultActual = Commands.run(jShell, "ls");
+
+    String actual = resultActual.getMessage();
+    
+    String expected = "";
+    
+    assertEquals(expected, actual);
   }
 
   @Test
   public void testMultiPathDir() {
-    JShellWindow jShell = new JShellWindow();
     Commands.run(jShell, "mkdir top top/middle1 top/middle2 top/middle1/bottom");
+    
     Result resultActual = Commands.run(jShell, "ls top");
-    String actual = resultActual.getMessage();
-    String expected = "/top:\nmiddle1\tmiddle2\n";
-    assertEquals(expected, actual);
-  }
-  
-  @Test
-  public void testNoArguments() {
-    JShellWindow jShell = new JShellWindow();
-    Result resultActual = Commands.run(jShell, "ls");
-    String actual = resultActual.getMessage();
-    String expected = "";
-    assertEquals(expected, actual);
-  }
 
+    String actual = resultActual.getMessage();
+
+    String expected = "/top:\nmiddle1\tmiddle2\n";
+
+    assertEquals(expected, actual);
+  }
 }
