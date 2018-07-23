b33d40bff2ae9e4698fc20d43ce4283a38d4c0ef
huan1085@UTORONTO.CA <huan1085@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-07-23 22:45:52 +0000

    added greptest

---
+++ Assignment2/src/test/GrepTest.java
@@ -0,0 +1,55 @@
+package test;
+
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+import commandsystem.Result;
+import commandsystem.Commands;
+import shell.JShellWindow;
+
+public class GrepTest {
+  @Before
+  public void setup() {
+    Commands.initializeCommandHashTable();
+  }
+  
+  @Test
+  public void recursiveCase() {
+    JShellWindow jShell = new JShellWindow();
+    Commands.run(jShell, "mkdir folder");
+    Commands.run(jShell, "cd folder");
+    Commands.run(jShell, "echo ne\nonetwo\ntwo\nonethree > test.txt");
+    Result test = Commands.run(jShell, "grep -R one folder");
+    String actual = test.getMessage();
+    String expected = ("onetwo\nonethree\n");
+    assertEquals(actual, expected);
+  }
+  
+  @Test
+  public void normalCase() {
+    JShellWindow jShell = new JShellWindow();
+    Commands.run(jShell, "echo a\nasd\ndd > file.txt");
+    Result test = Commands.run(jShell, "grep a file.txt");
+    String actual = test.getMessage();
+    String expected = ("a\nasd\n");
+    assertEquals(actual, expected);
+  }
+  
+  @Test
+  public void errorCase() {
+    JShellWindow jShell = new JShellWindow();
+    Result test = Commands.run(jShell, "grep a file.txt");
+    String actual = test.getErrorMessage();
+    String expected = ("The path does not exist.");
+    assertEquals(actual, expected);
+  }
+  
+  @Test
+  public void tooFewArgs() {
+    JShellWindow jShell = new JShellWindow();
+    Result test = Commands.run(jShell, "grep file.txt");
+    String actual = test.getErrorMessage();
+    String expected = ("Invalid number of arguments.");
+    assertEquals(actual, expected);
+  }
+}
