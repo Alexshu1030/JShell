30401fa906c803e7227b36ac77bae5ca2e12ef83
tanuanje@UTORONTO.CA <tanuanje@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-07-24 02:16:53 +0000

    added base setup and test cases

--- Assignment2/src/test/CpTest.java
+++ Assignment2/src/test/CpTest.java
@@ -1,5 +1,86 @@
 package test;
 
+import static org.junit.Assert.*;
+import java.lang.reflect.Field;
+import org.junit.*;
+import filesystem.*;
+import commandsystem.*;
+import exceptions.FileNotFoundException;
+import exceptions.InvalidPathException;
+import exceptions.PathExistsException;
+import shell.JShellWindow;
+
 public class CpTest {
 
+  private static JShellWindow jShell;
+  private static FileExplorer explorer;
+  
+  @BeforeClass
+  public static void setUpClass() {
+    
+    Commands.initializeCommandHashTable();
+  }
+  
+  @Before
+  public void setUp() {
+
+    jShell = new JShellWindow();
+    explorer = jShell.getFileExplorer();
+  }
+  
+  @After
+  public void tearDown() throws NoSuchFieldException, SecurityException,
+  IllegalArgumentException, IllegalAccessException {
+    // Reset the static self reference in the FileExplorer class
+    Field field = (explorer.getClass()).getDeclaredField("rootDirectory");
+    field.setAccessible(true);
+    field.set(null, null);
+  }
+  
+  @Test
+  public void testWithArgument() {
+    
+    Result resultActual = Commands.run(jShell, "cp A");
+    
+    String actualMessage = resultActual.getMessage();
+    String actualErrorMessage = resultActual.getErrorMessage();
+    
+    String expectedMessage = "";
+    String expectedErrorMessage = "Invalid number of arguments.";
+    
+    assertEquals(expectedMessage, actualMessage);
+    assertEquals(expectedErrorMessage, actualErrorMessage);
+  }
+  
+  @Test
+  public void testAtRootDirectory() {
+    
+    Result resultActual = Commands.run(jShell, "cp");
+    
+    String actualMessage = resultActual.getMessage();
+    String actualErrorMessage = resultActual.getErrorMessage();
+    
+    String expectedMessage = "/\n";
+    String expectedErrorMessage = "";
+    
+    assertEquals(expectedMessage, actualMessage);
+    assertEquals(expectedErrorMessage, actualErrorMessage);
+  }
+  
+  @Test
+  public void testAtNonRootDirectory() {
+    
+    Commands.run(jShell, "mkdir /A /A/A1 /A/A1/A11");
+    Commands.run(jShell, "cd /A/A1/A11");
+    Result resultActual = Commands.run(jShell, "cp");
+    
+    String actualMessage = resultActual.getMessage();
+    String actualErrorMessage = resultActual.getErrorMessage();
+    
+    String expectedMessage = "/A/A1/A11\n";
+    String expectedErrorMessage = "";
+    
+    assertEquals(expectedMessage, actualMessage);
+    assertEquals(expectedErrorMessage, actualErrorMessage);
+  }
 }
