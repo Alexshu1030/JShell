7760b96f57d10091862f7fec3a21b6388354fb31
hogancia <hogancia@UTORONTO.CA>
2018-07-24 01:47:59 +0000

    Created PushdTest.

---
+++ test/PushdTest.java
@@ -0,0 +1,108 @@
+package test;
+
+import static org.junit.Assert.*;
+import java.lang.reflect.Field;
+import org.junit.*;
+import filesystem.*;
+import commandsystem.*;
+import exceptions.FileNotFoundException;
+import exceptions.InvalidPathException;
+import exceptions.PathExistsException;
+import shell.JShellWindow;
+
+public class PushdTest {
+
+  private static JShellWindow jShell;
+  private static FileExplorer explorer;
+  
+  @BeforeClass
+  public static void setUpClass() {
+    
+    Commands.initializeCommandHashTable();
+  }
+  
+  @Before
+  public void setUp() {
+
+    jShell = new JShellWindow();
+    explorer = jShell.getFileExplorer();
+  }
+  
+  @After
+  public void tearDown() throws NoSuchFieldException, SecurityException,
+  IllegalArgumentException, IllegalAccessException {
+    // Reset the static self reference in the FileExplorer class
+    Field field = (explorer.getClass()).getDeclaredField("rootDirectory");
+    field.setAccessible(true);
+    field.set(null, null);
+  }
+  
+  @Test
+  public void testWithInvalidNumberOfArguments() {
+    
+    Result resultActual = Commands.run(jShell, "pushd A B");
+    
+    String actualMessage = resultActual.getMessage();
+    String actualErrorMessage = resultActual.getErrorMessage();
+    
+    String expectedMessage = "";
+    String expectedErrorMessage = "Invalid number of arguments.";
+    
+    assertEquals(expectedMessage, actualMessage);
+    assertEquals(expectedErrorMessage, actualErrorMessage);
+  }
+  
+  @Test
+  public void testPathThatDoesNotExist() {
+    
+    Result resultActual = Commands.run(jShell, "pushd A");
+    
+    String actualMessage = resultActual.getMessage();
+    String actualErrorMessage = resultActual.getErrorMessage();
+    
+    String expectedMessage = "";
+    String expectedErrorMessage = "The path does not exist. (Argument 0: A)";
+    
+    assertEquals(expectedMessage, actualMessage);
+    assertEquals(expectedErrorMessage, actualErrorMessage);
+  }
+  
+  @Test
+  public void testWithAbsolutePath() {
+    
+    Commands.run(jShell, "mkdir /A /A/A1 /A/A1/A11");
+    Result resultActual = Commands.run(jShell, "pushd /A/A1/A11");
+    
+    String actualMessage = resultActual.getMessage();
+    String actualErrorMessage = resultActual.getErrorMessage();
+    
+    String expectedMessage = "";
+    String expectedErrorMessage = "";
+    
+    assertEquals(expectedMessage, actualMessage);
+    assertEquals(expectedErrorMessage, actualErrorMessage);
+    
+    Result workingDirResult = Commands.run(jShell, "pwd");
+    assertEquals("/A/A1/A11\n", workingDirResult.getMessage());
+  }
+  
+  @Test
+  public void testWithRelativePath() {
+    
+    Commands.run(jShell, "mkdir /A /A/A1 /A/A1/A11");
+    Commands.run(jShell, "cd /A");
+    Result resultActual = Commands.run(jShell, "pushd A1/A11");
+    
+    String actualMessage = resultActual.getMessage();
+    String actualErrorMessage = resultActual.getErrorMessage();
+    
+    String expectedMessage = "";
+    String expectedErrorMessage = "";
+    
+    assertEquals(expectedMessage, actualMessage);
+    assertEquals(expectedErrorMessage, actualErrorMessage);
+    
+    Result workingDirResult = Commands.run(jShell, "pwd");
+    assertEquals("/A/A1/A11\n", workingDirResult.getMessage());
+  }
+}
