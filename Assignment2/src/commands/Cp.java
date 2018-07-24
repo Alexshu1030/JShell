439ae81078679316174585a3616c6c246be1553a
huan1085@UTORONTO.CA <huan1085@UTORONTO.CA@da5444df-959f-4216-bafe-4c3c28ebda28>
2018-07-24 03:00:51 +0000

    added documentation to cp

--- Assignment2/src/commands/Cp.java
+++ Assignment2/src/commands/Cp.java
@@ -36,47 +36,59 @@ import exceptions.*;
 import filesystem.*;
 import shell.JShellWindow;
 
-public class Cp implements Command{
-  
+public class Cp implements Command {
+
+  /**
+   * commandName The command name of the current class
+   */
   private String commandName = "cp";
+  /**
+   * helpText The help text for the current class
+   */
   private String helpText = "";
 
+  /**
+   * Returns true if execution of the command is successful. Execution changes
+   * current directory to the one specified in arguments
+   * 
+   * @param jShell the window that will be printed on
+   * @param arguments a list containing the directory to be changed into
+   * @return result the output to the shell
+   */
   public Result run(JShellWindow jShell, ArrayList<String> arguments) {
-    
+
     Result result = areValidArguments(arguments);
-    
+
     // Ensure that the arguments are valid
     if (!result.errorOccured()) {
-      
+
       FileExplorer explorer = jShell.getFileExplorer();
-      
+
       // Get the arguments
       String oldPath = arguments.get(0);
       String newPath = arguments.get(1);
-      
+
       File oldFile = null;
-      
+
       // Try to get the file we are copying
       try {
         oldFile = explorer.getFile(oldPath);
-      }
-      catch (FileNotFoundException e) {
+      } catch (FileNotFoundException e) {
         result.logError(0, "The old path does not exist.");
       }
-      
+
       if (oldFile != null) {
         if (oldFile.isDirectory()) {
           // Get the files in the directory
-          ArrayList<File> files = (ArrayList<File>)((Directory)
-              oldFile).getFileContents();
+          ArrayList<File> files =
+              (ArrayList<File>) ((Directory) oldFile).getFileContents();
           // Iterate over the files in the directory and copy them
           for (int i = 0; i < files.size(); i++) {
             // Get the current file and create a copy of it at the new path
             File currentFile = files.get(i);
             copyFile(explorer, result, currentFile, newPath);
           }
-        }
-        else {
+        } else {
           // Get the old file and create a copy of it at the new path
           copyFile(explorer, result, oldFile, newPath);
         }
@@ -84,49 +96,76 @@ public class Cp implements Command{
     }
     return result;
   }
-  
-  private void copyFile(FileExplorer explorer, Result result, 
-      File originalFile, String path) {
-    
+
+  /**
+   * Copies a file to a new location indicated by path
+   * 
+   * @param explorer the window that we're working with
+   * @param result result object used to log errors
+   * @param originalFile the original file that we're copying
+   * @param path the path that we're copying into
+   */
+  private void copyFile(FileExplorer explorer, Result result, File originalFile,
+      String path) {
+
     // We have create a copy of the old file. This is a free floating
     // file and is not in any directory right now.
     File fileCopy = originalFile.createCopy();
     // Add the file into the directory at the new path
     try {
-    explorer.addFile(path, fileCopy);
-    }
-    catch (PathExistsException e) {
-      result.logError(1, "A file of the same name already exists at" +
-          " the new path.");
-    }
-    catch (InvalidPathException e) {
+      explorer.addFile(path, fileCopy);
+    } catch (PathExistsException e) {
+      result.logError(1,
+          "A file of the same name already exists at" + " the new path.");
+    } catch (InvalidPathException e) {
       result.logError(1, "Invalid path.");
     }
   }
 
+  /**
+   * Checks if the arguments given to the command is valid
+   * 
+   * @param arguments the list of str arguments passed to the command
+   * @return isValid true if the command is valid and vice versa
+   */
   public Result areValidArguments(ArrayList<String> arguments) {
-    
+    // setup a new result object
     Result result = new Result(arguments);
-    
+    // log error if the # of arguments are >2
     if (arguments.size() != 2) {
       result.logError("Invalid number of arguments.");
     }
-    
+
     return result;
   }
-  
+
+  /**
+   * Returns the name of this command
+   * 
+   * @return commandName this is the command's name
+   */
   public String getCommandName() {
-    
+
     return commandName;
   }
 
+  /**
+   * Returns the help text for this command.
+   * 
+   * @return the help text for this command
+   */
   public String getHelpText() {
-    
+
     return helpText;
   }
 
+  /**
+   * Returns whether if the output can be redirected or not
+   * 
+   * @return whether if the output can be redirected or not
+   */
   public boolean canBeRedirected() {
-    
+
     return true;
   }
 
